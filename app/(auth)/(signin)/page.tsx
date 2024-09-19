7596b586b212189b10f27092a684e025be922ccf
gabiroarnaud <gabiroarnaud@gmail.com>
2024-09-19 11:00:07 +0000

    Some changes

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -21,8 +21,14 @@ export default function AuthenticationPage() {
       >
         Login
       </Link>
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
+
+      {/* Background div with farmer image */}
+      <div
+        className="relative hidden h-full flex-col bg-cover bg-center p-10 text-white lg:flex dark:border-r"
+        style={{ backgroundImage: 'url(/images/farmer.jpg)' }}
+      >
+        <div className="absolute inset-0 bg-zinc-900/50" />{' '}
+        {/* semi-transparent overlay */}
         <div className="relative z-20 flex items-center text-lg font-medium">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +47,7 @@ export default function AuthenticationPage() {
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;Dataseed Africa is africa&apos;s largest agriculture
+              &ldquo;Dataseed Africa is Africa&apos;s largest agriculture
               dataset with information ranging from farm status, farmers
               information, agricultural supply chains and so much more.&rdquo;
             </p>
@@ -49,6 +55,8 @@ export default function AuthenticationPage() {
           </blockquote>
         </div>
       </div>
+
+      {/* Authentication form */}
       <div className="flex h-full items-center p-4 lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
