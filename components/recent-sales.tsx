f924ba3265730ca04242d0501a43b1f182a738f1
gabiroarnaud <gabiroarnaud@gmail.com>
2024-09-25 12:27:13 +0000

    Dummy data added

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -9,7 +9,7 @@ export function RecentSales() {
     <div className="space-y-8">
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="../public/images/wheat.jpg" alt="Avatar" />
+          <AvatarImage src="/images/wheat.jpg" alt="Avatar" />
           <AvatarFallback>CR</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -22,7 +22,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage src="../public/images/tubers.avif" alt="Avatar" />
+          <AvatarImage src="/public/images/tubers.avif" alt="Avatar" />
           <AvatarFallback>TB</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
